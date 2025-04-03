1a7ecef5fe263ae00e283b6f02afba82d2ede075
JunyaShimmura <tattat92929@gmail.com>
2025-04-03 20:59:40 +0900

    setTimeout　追加　UIブロック回避

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,29 +1,29 @@
 function initMap() {
+    console.log("位置表示処理を実行！");
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
 
             var lat = position.coords.latitude;
             var lng = position.coords.longitude;
             document.getElementById("locationText").innerText = `緯度: ${lat}, 経度: ${lng}`;
-
-            var map = new google.maps.Map(document.getElementById('map'), {
-                center: { lat: lat, lng: lng },
-                zoom: 15
-            });
-
-            new google.maps.Marker({
-                position: { lat: lat, lng: lng },
-                map: map,
-                title: "現在地"
-            });
-
-
+            // setTimeoutで描画を遅らせてUIブロック回避
+            setTimeout(() => {
+                console.log("位置の描画開始！");
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    center: { lat: lat, lng: lng },
+                    zoom: 15
+                });
+                new google.maps.Marker({
+                    position: { lat: lat, lng: lng },
+                    map: map,
+                    title: "現在地"
+                });
+            }, 10)
         });
     } else {
         alert("位置情報を取得できません");
     }
     document. getElementById('locationText'). textContent = '位置情報取得中...';
-    console.log("位置表示処理を実行！");
 //    {           enableHighAccuracy: false,  // 高精度OFF（速くする）
 //                timeout: 15000,              // 15秒でタイムアウト
 //                maximumAge: 60000     }      // 1分以内ならキャッシュ利用　　
@@ -61,7 +61,7 @@ function getLocation(button) {
                 form.submit() ;
             },
             function(error) {
-                alert("位置情報をーーー取得できませんでした: " + error.message);
+                alert("位置情報を取得できませんでした: " + error.message);
                  // 失敗してもフォームを送信
                  form.submit();
             },
@@ -71,13 +71,13 @@ function getLocation(button) {
             }
         );
     } else {
-        alert("このブラウザは----位置情報をサポートしていません。");
+        alert("このブラウザは位置情報をサポートしていません。");
          // 失敗してもフォームを送信
          form.submit();
     }
 
 }
-
+// ボタンに応じた遷移先URLを設定しsubmitする
 function submitLocation(button){
     getLocation();
     let url = button.getAttribute("dataUrl")
